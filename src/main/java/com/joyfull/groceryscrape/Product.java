f7bd959838e1b819b503ec57a3382b7ecc9a6d61
joydore <joy.dore@isotom.com>
2016-07-31 10:46:48 +0100

    initial commit

---
+++ src/main/java/com/joyfull/groceryscrape/Product.java
@@ -0,0 +1,93 @@
+package com.joyfull.groceryscrape;
+
+/**
+ * @author Joy
+ *
+ */
+public class Product {
+
+	private String title;
+	private long file_size;
+	private double unit_price;
+	private String description;
+	
+	/**
+	 * @param title
+	 * @param file_size
+	 * @param unit_price
+	 * @param description
+	 */
+	public Product (String title, long file_size, double unit_price, String description) {
+		this.title = title;
+		this.file_size = file_size;
+		this.unit_price = unit_price;
+		this.description = description;
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * @return the file_size
+	 */
+	public long getFile_size() {
+		return file_size;
+	}
+
+	/**
+	 * @param file_size the file_size to set
+	 */
+	public void setFile_size(long file_size) {
+		this.file_size = file_size;
+	}
+
+	/**
+	 * @return the unit_price
+	 */
+	public double getUnit_price() {
+		return unit_price;
+	}
+
+	/**
+	 * @param unit_price the unit_price to set
+	 */
+	public void setUnit_price(double unit_price) {
+		this.unit_price = unit_price;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Product [title=" + title + ", file_size=" + file_size
+				+ ", unit_price=" + unit_price + ", description=" + description
+				+ "]";
+	}
+
+}
---
+++ src/main/java/com/joyfull/groceryscrape/ProductList.java
@@ -0,0 +1,41 @@
+package com.joyfull.groceryscrape;
+
+import java.util.List; 
+
+/**
+ * @author Joy
+ *
+ */
+public class ProductList {
+	private List<Product> products;
+
+	/**
+	 * @param products
+	 */
+	public ProductList(List<Product> products) {
+		this.products = products;
+	}
+	
+	/**
+	 * 
+	 * @return total of all unit prices
+	 */
+	public double getTotal() {
+		double total = 0;
+		for (Product prod : this.products){
+			total += prod.getUnit_price();
+		}
+		return total;
+	}
+	
+	public String toString(){
+		StringBuilder str = new StringBuilder(300);
+		str.append("Products:");
+		for (Product prod : this.products) {
+			str.append(prod.toString() + " ");
+		}
+		str.append("Total=" + this.getTotal());
+		return str.toString();
+	}
+
+}
